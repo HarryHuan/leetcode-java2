227b2e4ad78444fc0e61d7f1386648e7898ca1e7
HarryHuan <963548198@qq.com>
2021-08-10 17:58:07 +0800

    firstJava-leetcode

--- src/com/harry/leetcode/editor/cn/ImplementRand10UsingRand7.java
+++ src/com/harry/leetcode/editor/cn/ImplementRand10UsingRand7.java
@@ -64,7 +64,13 @@ public class ImplementRand10UsingRand7{
  */
 class Solution extends SolBase {
     public int rand10() {
-        
+        int row, col, idx;
+        do{
+        	row = rand7();
+        	col = rand7();
+        	idx = col + (row - 1) * 7;
+		}while (idx > 40);
+        return 1 + (idx - 1) % 10;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
